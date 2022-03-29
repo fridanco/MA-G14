2792bdcc1e7ee051f76b192a5588185570e3d9bc
AleKrios7 <alex.frenk117@gmail.com>
2022-03-29 09:34:32 +0200

    initial commit

---
+++ app/src/main/java/it/polito/mad/g14/timebank/MainActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.mad.g14.timebank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
