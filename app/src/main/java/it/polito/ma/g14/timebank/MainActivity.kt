8eb91b837dec82c5853ffe451578d098a31cccf8
fridanco <francesco.difranco97@gmail.com>
2022-03-29 17:38:45 +0200

    main activity partially done

--- app/src/main/java/it/polito/ma/g14/timebank/MainActivity.kt
+++ app/src/main/java/it/polito/ma/g14/timebank/MainActivity.kt
@@ -2,10 +2,23 @@ package it.polito.ma.g14.timebank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val fullName :  String = "Mario";
+    val email : String = "m@gmail.com"
+    val nickName : String = "mariodedapolito"
+    val location : String = "location"
+    var skills = mutableListOf<String>();
+    val description : String = "";
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.textView4).text = fullName
+        findViewById<TextView>(R.id.textView5).text = nickName
+        findViewById<TextView>(R.id.textView6).text = email
+        findViewById<TextView>(R.id.textView7).text = location
     }
 }
